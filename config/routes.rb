468eb29169b015678c1807588c49de8656204023
taketoyamori <ymymz87@icloud.com>
2020-11-30 11:53:09 +0000

    [add] model controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#top'
+  get '/about' => 'home#about'
+  devise_for :users
+  resources :users, only: [:index, :edit, :show, :update, ]
+  resources :books, only: [:index, :create, :edit, :show, :update, :destroy]
 end
